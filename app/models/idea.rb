68ec5257535f649e3ac9ef2c0fd6f29e3771ba2c
Adam Caron <adamwcaron@gmail.com>
2015-10-30 23:03:11 -0600

    Cleanup routes and remove psudocode

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,21 +2,4 @@ class Idea < ActiveRecord::Base
   validates :title, :body, presence: true
 
   enum quality: [:swill, :plausible, :genius]
-
-  # def increase_quality
-  #   if swill?
-  #     plausible!
-  #   elsif plausible?
-  #     genius!
-  #   end
-  # end
-
-  # def decrease_quality
-  #   if genius?
-  #     plausible!
-  #   elsif plausible?
-  #     swill!
-  #   end
-  # end
-
 end
