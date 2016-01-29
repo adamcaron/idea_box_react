2abdf6dfb723fc7a884ba04ac030dbbbf43ae460
Adam Caron <adamwcaron@gmail.com>
2016-01-29 09:39:06 -0700

    Create a new idea
    also, sort ideas on the server in descening
    order, before sending to client

--- app/assets/javascripts/components/_dashboard.js.jsx
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -6,19 +6,34 @@ var Dashboard = React.createClass({
     $.ajax({
       url: '/api/v1/ideas.json',
       type: 'GET',
-      success: function (response) {
+      success: (response) => {
         this.setState({ideas: response});
-      }.bind(this),
+      },
       error: function () {
-        console.log('Sorry, didn\'t work!')
+        console.log('Sorry, could not fetch the ideas.')
       }
     });
+  },
+  createIdea: function (idea) {
+    $.ajax({
+      url: '/api/v1/ideas.json',
+      type: 'POST',
+      data: { idea: { title: idea.title, body: idea.body } },
+      success: (newIdea) => {
+        this.state.ideas.unshift(newIdea)
+        this.setState({ ideas: this.state.ideas });
+      },
+      error: function(response) {
+        console.log(response);
+        alert('Sorry, the idea could not be created!');
+      }
+    })
   },
   render: function () {
     return (
       <div className='flex-container'>
         <Ideas ideas={this.state.ideas} />
-        <IdeaCreation />
+        <NewIdea createIdea={this.createIdea} />
       </div>
     );
   }
@@ -81,18 +96,31 @@ var IdeaIndex = React.createClass({
   }
 });
 
-var IdeaCreation = React.createClass({
+var NewIdea = React.createClass({
+  createIdea: function (event) {
+    event.preventDefault();
+
+    var title = React.findDOMNode(this.refs.title).value.trim();
+    var body = React.findDOMNode(this.refs.body).value.trim();
+
+    var idea = {
+      title: title,
+      body: body
+    }
+
+    this.props.createIdea(idea);
+    React.findDOMNode(this.refs.newIdeaForm).reset();
+  },
   render: function () {
     return (
       <aside id='idea-creation-area'>
         <h2>Add an Idea:</h2>
-        <form action='/' acceptCharset='UTF-8' data-remote='true' method='post'>
-          <input name='utf8' type='hidden' value='&#x2713;' />
+        <form ref='newIdeaForm' onSubmit={this.createIdea}>
           <label htmlFor='idea_title'>Title</label>
-          <input type='text' name='idea[title]' id='idea_title' />
+          <input type='text' ref='title' name='idea[title]' id='idea_title' placeholder='Title'/>
           <label htmlFor='idea_body'>Body</label>
-          <textarea name='idea[body]' id='idea_body'></textarea>
-          <input type='submit' name='commit' value='Save' id='save-idea' />
+          <textarea type='text' ref='body' name='idea[body]' id='idea_body' placeholder='Description'></textarea>
+          <button type='submit' id='save-idea'>Create Idea</button>
         </form>
       </aside>
     );
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::IdeasController < Api::V1::BaseController
   def index
-    respond_with Idea.all
+    respond_with Idea.all.sort_by(&:created_at).reverse
+  end
+
+  def create
+    @idea = Idea.create(idea_params)
+    respond_with :api, :v1, @idea, location: nil
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
   end
 end
